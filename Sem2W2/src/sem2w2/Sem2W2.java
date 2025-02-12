0e9ad7d88ff006e3fdd908ad6e7038e222aa3753
SuryaRizki06 <surya.rizki123456789@gmail.com>
2025-02-12 20:33:01 +0700

    surya

---
+++ Sem2W2/src/sem2w2/Sem2W2.java
@@ -0,0 +1,17 @@
+
+package sem2w2;
+
+public class Sem2W2 {
+
+    public static void main(String[] args) {
+         String fName ,stuStatus;
+         fName ="Lisa palombo";
+        int IName =123456789;
+        stuStatus = "Active";
+         System.out.println(fName);
+         System.out.println(IName);
+         System.out.println(stuStatus);
+         
+    }
+    
+}
---
+++ contoh/src/contoh/Contoh.java
@@ -0,0 +1,11 @@
+
+package contoh;
+
+public class Contoh {
+
+    public static void main(String[] args) {
+String halo ="halo semuanya saya surya ";
+        System.out.println(halo);
+    }
+    
+}
